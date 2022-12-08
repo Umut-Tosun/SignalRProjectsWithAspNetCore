3bc1e633a6e1211032987f0d56e2e2fb5bdd56e6
Umut Tosun <tosun.umutt@gmail.com>
2022-12-08 22:31:30 +0300

    Add project files.

---
+++ SignalRFirstProjectWithAspNetCore/Hubs/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRFirstProjectWithAspNetCore.Hubs
+{
+    public class MyHub : Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+          await  Clients.All.SendAsync("receiveMessage",message);
+        }
+    }
+}
