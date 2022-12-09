7e090a79d9c964201fb420e15306e22b9801634e
Umut Tosun <tosun.umutt@gmail.com>
2022-12-09 23:17:20 +0300

    Connection Events and clients list method added.

--- SignalRFirstProjectWithAspNetCore/Hubs/MyHub.cs
+++ SignalRFirstProjectWithAspNetCore/Hubs/MyHub.cs
@@ -8,9 +8,22 @@ namespace SignalRFirstProjectWithAspNetCore.Hubs
 {
     public class MyHub : Hub
     {
+        static List<string> clients = new List<string>();
         public async Task SendMessageAsync(string message)
         {
-          await  Clients.All.SendAsync("receiveMessage",message);
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
         }
     }
 }
