954f34a7dede2466a403689eb9469ba9c67edde3
Umut Tosun <tosun.umutt@gmail.com>
2022-12-10 20:38:25 +0300

    MyBusiness added for demo businessLayer.

---
+++ SignalRFirstProjectWithAspNetCore/Business/MyBusiness.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRFirstProjectWithAspNetCore.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRFirstProjectWithAspNetCore.Business
+{
+    public class MyBusiness
+    {
+        IHubContext<MyHub> _hubContext;
+
+        public MyBusiness(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            await _hubContext.Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
