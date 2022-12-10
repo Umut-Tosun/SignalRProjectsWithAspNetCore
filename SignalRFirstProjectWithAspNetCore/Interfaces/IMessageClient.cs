f9ea2f8068bd75ee8e6d60f5f932e706ffd410ef
Umut Tosun <tosun.umutt@gmail.com>
2022-12-10 20:35:54 +0300

    IMessageClient created for MyHub actions.

---
+++ SignalRFirstProjectWithAspNetCore/Interfaces/IMessageClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRFirstProjectWithAspNetCore.Interfaces
+{
+    public interface IMessageClient
+    {
+        Task Clients(List<string> clients);
+        Task UserJoined(string connectionId);
+        Task UserLeaved(string connectionId);
+    }
+}
