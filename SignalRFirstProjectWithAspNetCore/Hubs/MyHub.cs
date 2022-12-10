0f47478c4d40f2e37a2842c545fd8406b857ee0d
Umut Tosun <tosun.umutt@gmail.com>
2022-12-10 20:37:43 +0300

    Some structures changed .

--- SignalRFirstProjectWithAspNetCore/Hubs/MyHub.cs
+++ SignalRFirstProjectWithAspNetCore/Hubs/MyHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRFirstProjectWithAspNetCore.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,24 +7,28 @@ using System.Threading.Tasks;
 
 namespace SignalRFirstProjectWithAspNetCore.Hubs
 {
-    public class MyHub : Hub
+    public class MyHub : Hub<IMessageClient>
     {
         static List<string> clients = new List<string>();
-        public async Task SendMessageAsync(string message)
-        {
-            await Clients.All.SendAsync("receiveMessage", message);
-        }
+        //public async Task SendMessageAsync(string message)
+        //{
+        //    await Clients.All.SendAsync("receiveMessage", message);
+        //}
         public override async Task OnConnectedAsync()
         {
             clients.Add(Context.ConnectionId);
-            await Clients.All.SendAsync("clients", clients);
-            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+            //await Clients.All.SendAsync("clients", clients);
+            //await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserJoined(Context.ConnectionId);
         }
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             clients.Remove(Context.ConnectionId);
-            await Clients.All.SendAsync("clients", clients);
-            await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
+            //await Clients.All.SendAsync("clients", clients);
+            //await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
+            await Clients.All.Clients(clients);
+            await Clients.All.UserLeaved(Context.ConnectionId);
         }
     }
 }
